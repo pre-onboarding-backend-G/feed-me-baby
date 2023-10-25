ea08db9c4495d67c3202d1fb34ea69c1deb6276c
Zamoca <suntail2002@naver.com>
2023-10-25 16:46:39 +0900

    Chore: 환경변수 컨트롤 및 검증 설정 적용 #1
    - 일관성 있는 개발환경을 위해서 환경변수 컨트롤하고 검증하는 미들웨어를 설정
    
    Related to #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './common/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+      validate,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/env.validation.ts
@@ -0,0 +1,35 @@
+import { plainToClass } from 'class-transformer';
+import { IsEnum, IsString, validateSync } from 'class-validator';
+
+enum Environment {
+  Development = 'development',
+  Production = 'production',
+  Local = 'local',
+}
+
+class EnvironmentVariables {
+  @IsEnum(Environment)
+  NODE_ENV: Environment;
+
+  @IsString()
+  ALLOWED_ORIGINS: string;
+
+  @IsString()
+  DYNAMODB_TABLE_PREFIX: string;
+}
+
+export function validate(
+  config: Record<string, unknown>,
+): EnvironmentVariables {
+  const validatedConfig = plainToClass(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
+}
