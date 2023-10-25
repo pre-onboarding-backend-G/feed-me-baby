86e5c772baf4834ae30ebb0905d61ffb38a2cb6b
Zamoca <suntail2002@naver.com>
2023-10-25 16:49:55 +0900

    Feat: global exception filter 적용 #1
    - 서비스 로직에서 간단한 예외처리를 위해 전역 예외 필터 설정 적용
    
    Related to #1

---
+++ src/common/filter/http-exception.filter.ts
@@ -0,0 +1,41 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  Logger,
+  BadRequestException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name);
+  catch(exception: HttpException, host: ArgumentsHost): void {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    this.logger.error(
+      `Request URL: ${request.url} | Status: ${status} | Message: ${
+        exception.message || 'Internal server error'
+      }`,
+    );
+
+    if (exception instanceof BadRequestException) {
+      response.status(exception.getStatus()).json(exception);
+      return;
+    }
+    response.status(status).json({
+      statusCode: status,
+      message: exception.message || 'Internal server error',
+      path: request.url,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filter/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
