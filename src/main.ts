040c57196742bec7a65c4d435ad0b2532d14f115
Zamoca <suntail2002@naver.com>
2023-10-30 22:21:35 +0900

    Refactor: 함수 타입 반환, 설정 변경
    - main.ts에서 함수 타입을 반환하고 가독성 좋게 변경해봤습니다.
    Related to #5

--- src/main.ts
+++ src/main.ts
@@ -4,36 +4,36 @@ import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+  
+  const validationPipeOptions = {
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true,
+  }
+
+  const corsOptions = {
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  }
+
   const config = new DocumentBuilder()
     .setTitle('Example API')
     .setDescription('The Example API description')
     .setVersion('1.0')
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-
-  app.useGlobalFilters(new HttpExceptionFilter());
-
-  app.enableCors({
-    origin: allowedOrigins,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-  });
-
+  app
+    .useGlobalPipes(new ValidationPipe(validationPipeOptions))
+    .useGlobalFilters(new HttpExceptionFilter())
+    .enableCors(corsOptions)
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
