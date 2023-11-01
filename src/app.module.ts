4e1efc40da4b4d6786f2017e74c949a9af0fd742
Zamoca <suntail2002@naver.com>
2023-11-01 14:21:55 +0900

    Chore: pgadmin, pg, typeorm 설정
    - postgresql 설치
    - postgresql을 모니터링용 pgadmin 설치
    - typeorm 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './common/env.validation';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -10,6 +11,14 @@ import { validate } from './common/env.validation';
       envFilePath: '.env',
       isGlobal: true,
       validate,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS,
+      database: process.env.DATABASE_NAME,
     }),
   ],
   controllers: [AppController],
