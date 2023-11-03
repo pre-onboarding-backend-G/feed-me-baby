94702b5565a3ff689a9759ad530e4112942cb3d1
Sang Un Lee <sangunlee6@gmail.com>
2023-11-03 14:22:01 +0900

    Chore: module에 repository 주입
    - RestaurantGuideModule에 RestaurantGuideRepository를 주입
    
    Related to #2

--- src/restaurant-guide/repository/restaurant-guide.repository.ts
+++ src/restaurant-guide/repository/restaurant-guide.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class RestaurantGuideService {
+export class RestaurantGuideRepository {
   // restaurant entity
   // review entity
   /**
