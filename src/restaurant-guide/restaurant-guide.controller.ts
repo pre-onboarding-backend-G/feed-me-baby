93f80e8bf743cf8903d12a3c4688eca880c4b63f
Zamoca <suntail2002@naver.com>
2023-11-04 00:17:06 +0900

    Feat: 서비스, 컨트롤러 작성
    - 서비스, 컨트롤러 작성
    
    Related to #4

--- src/restaurant-guide/restaurant-guide.controller.ts
+++ src/restaurant-guide/restaurant-guide.controller.ts
@@ -1,11 +1,12 @@
 import { Controller } from '@nestjs/common';
 import { RestaurantGuideService } from './restaurant-guide.service';
+import { RequestCoordinateWithRangeDto } from './dto/coordinate-req.dto';
 
 @Controller('restaurant-guide')
 export class RestaurantGuideController {
   constructor(
     private readonly restaurantGuideService: RestaurantGuideService,
-  ) {}
+  ) { }
   /**
    * @author Yeon Kyu
    * @email suntail2002@naver.com
@@ -13,7 +14,14 @@ export class RestaurantGuideController {
    * @modify date 2023-11-01 22:56:10
    * @desc [description]
    */
+
+  async getRestaurantListByQuery(request: RequestCoordinateWithRangeDto): Promise<void> {
+    await this.restaurantGuideService.getRestaurantList(request)
+  }
+
   /**
+   * 
+   * 
    *
    *
    *
