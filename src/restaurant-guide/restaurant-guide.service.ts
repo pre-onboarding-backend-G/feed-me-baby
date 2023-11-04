001b7a981c0ab15803b2d3a10cfa5c5f55f17de6
Zamoca <suntail2002@naver.com>
2023-11-05 02:36:13 +0900

    Refactor: 목록 조회 서비스 레이어 예외, 필터링 추가
    - 위도, 경도가 없을 때의 예외처리를 추가하고 사각형 -> 원의 범위로 필터링 합니다
    
    Related to #4

--- src/restaurant-guide/restaurant-guide.service.ts
+++ src/restaurant-guide/restaurant-guide.service.ts
@@ -1,42 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { RequestCoordinateWithRangeDto } from './dto/coordinate-req.dto';
 import { RestaurantGuideRepository } from './repository/restaurant-guide.repository';
 import { Restaurant } from 'src/restaurant/entity/restaurant.entity';
-import { GetRestaurantListDto } from './dto/get-restaurant.dto';
+import { GetRestaurantsDto } from './dto/get-restaurant.dto';
 
 @Injectable()
 export class RestaurantGuideService {
   constructor(
     private readonly restaurantGuideRepository: RestaurantGuideRepository,
   ) {}
-  //리포지토리 주입
   /**
    * @author Yeon Kyu
    * @email suntail2002@naver.com
    * @create date 2023-11-01 22:56:10
    * @modify date 2023-11-01 22:56:10
-   * @desc [description]
+   * @desc 쿼리 파라미터에서 지정한 위도, 경도, 범위 값을 가지고
+   *       맛집 목록을 가져온 다음 원의 크기로 필터링 합니다.
    */
 
   async getRestaurantList(
     request: RequestCoordinateWithRangeDto,
-  ): Promise<GetRestaurantListDto[]> {
+  ): Promise<GetRestaurantsDto[]> {
     const { lat, lon, validateRange } = request;
+
+    // TODO: 위, 경도 값이 주어지지 않았을 때 유저의 위치값 넣기
+
+    if (!lat || !lon) {
+      throw new BadRequestException('사용자의 위치 정보가 없습니다');
+    }
+
     const restaurants =
       await this.restaurantGuideRepository.findRestaurantsInRange(
         lat,
         lon,
         validateRange,
       );
-    return restaurants.map(
-      (restaurant) =>
-        new GetRestaurantListDto(
-          restaurant.id,
-          restaurant.name,
-          restaurant.lat,
-          restaurant.lon,
-        ),
-    );
+
+    return restaurants
+      .filter((restaurant) => {
+        const distance1 = Math.sqrt(
+          Math.pow(Math.abs(lat - restaurant.lat), 2) +
+            Math.pow(Math.abs(lon - restaurant.lon), 2),
+        );
+        const distance2 = Math.sqrt(Math.pow(validateRange, 2));
+
+        return distance1 <= distance2;
+      })
+      .map((restaurant) => new GetRestaurantsDto(restaurant));
   }
   /**
    *
