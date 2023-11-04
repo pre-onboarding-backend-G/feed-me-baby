733ee46a94684203af8df6a87789358b2f8fafd8
Zamoca <suntail2002@naver.com>
2023-11-05 02:34:35 +0900

    Feat: 목록 조회 swagger 문서화
    - 쿼리 파라미터, 200 성공, 400 에러에 관한 문서 추가
    
    Related to #4

--- src/restaurant-guide/restaurant-guide.controller.ts
+++ src/restaurant-guide/restaurant-guide.controller.ts
@@ -2,28 +2,44 @@ import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { RestaurantGuideService } from './restaurant-guide.service';
 import { RequestCoordinateWithRangeDto } from './dto/coordinate-req.dto';
 import { Restaurant } from '../restaurant/entity/restaurant.entity';
-import { GeoJsonResponseDto } from './dto/geojson-response.dto';
-import { GetRestaurantListDto } from './dto/get-restaurant.dto';
+import { GeoJsonResponse } from './dto/geojson-res.dto';
+import { GetRestaurantsDto } from './dto/get-restaurant.dto';
+import { ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
 
 @Controller('restaurant-guide')
 export class RestaurantGuideController {
   constructor(
     private readonly restaurantGuideService: RestaurantGuideService,
-  ) {}
+  ) { }
   /**
    * @author Yeon Kyu
    * @email suntail2002@naver.com
    * @create date 2023-11-01 22:56:10
    * @modify date 2023-11-01 22:56:10
-   * @desc [description]
+   * @desc GeoJson에서 볼 수 있게 데이터 포맷을 변경합니다.
    */
   @Get()
+  @ApiOperation({ summary: '주변 맛집 정보들을 가져옵니다' })
+  @ApiQuery({ name: 'lat', type: Number, description: '위도 값' })
+  @ApiQuery({ name: 'lon', type: Number, description: '경도 값' })
+  @ApiQuery({
+    name: 'range',
+    type: Number,
+    description: '찾고싶은 범위 0.1(약 11km) 이하의 값을 입력(기본 값: 0.1)',
+  })
+  @ApiResponse({
+    status: 200,
+    type: GetRestaurantsDto,
+    isArray: true,
+    description: 'geojson에 넣을 수 있는 데이터 포맷을 따릅니다',
+  })
+  @ApiResponse({ status: 400, description: '위도나 경도 값이 없습니다' })
   async getRestaurantListByQuery(
     @Query() request: RequestCoordinateWithRangeDto,
-  ): Promise<GeoJsonResponseDto<GetRestaurantListDto>> {
+  ): Promise<GeoJsonResponse<GetRestaurantsDto>> {
     const restaurantList =
       await this.restaurantGuideService.getRestaurantList(request);
-    return new GeoJsonResponseDto<GetRestaurantListDto>(restaurantList);
+    return new GeoJsonResponse<GetRestaurantsDto>(restaurantList);
   }
 
   /**
