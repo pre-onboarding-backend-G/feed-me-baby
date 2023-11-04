76d9be45fec321f0e664be7085984245c4cf393d
Zamoca <suntail2002@naver.com>
2023-11-04 23:54:22 +0900

    Feat: typeorm 모듈 추가
    - restaurant, city, category entity를 가져오기
    
    Related to #4

--- src/restaurant-guide/restaurant-guide.module.ts
+++ src/restaurant-guide/restaurant-guide.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { RestaurantGuideService } from './restaurant-guide.service';
 import { RestaurantGuideController } from './restaurant-guide.controller';
 import { RestaurantGuideRepository } from './repository/restaurant-guide.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from 'src/restaurant/entity/restaurant.entity';
+import { Category } from 'src/restaurant/entity/category.entity';
+import { City } from 'src/restaurant/entity/city.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Restaurant, Category, City])],
   controllers: [RestaurantGuideController],
   providers: [RestaurantGuideService, RestaurantGuideRepository],
 })
