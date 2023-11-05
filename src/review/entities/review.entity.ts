954e0f5220679515a4d23404b8adfbe159b7f52a
Refi Go <64242999+refigo@users.noreply.github.com>
2023-11-05 09:27:40 +0900

    Refactor : Modify Review Entity #6

--- src/review/entities/review.entity.ts
+++ src/review/entities/review.entity.ts
@@ -1,29 +1,43 @@
-import { Column, CreateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { RestaurantReviewAggregation } from "./restaurant-review-aggregation.entity";
 
+@Entity()
 export class Review {
-	@PrimaryGeneratedColumn({ name: 'id', type: 'bigint' })
+	@PrimaryGeneratedColumn({ name: 'id', type: 'bigint', unsigned: true })
 	id: number;
 
-	@Column({ name: 'content', type: 'varchar'})
-	content: string;
+	@Column({ name: 'content', type: 'varchar', nullable: true, length: 250 })
+	content: string | null;
 
-	@Column({ name: 'score', type: 'tinyint'})
-	score: number;	
+	@Column({ name: 'score', type: 'tinyint', nullable: false })
+	score: number;
 
-	// @ManyToOne(() => UserEntity)
+	@CreateDateColumn({
+		name: 'created_at',
+		type: 'timestamp',
+		nullable: false,
+	})
+	createdAt: Date;
+
+	@UpdateDateColumn({
+		name: 'updated_at', 
+		type: 'timestamp', 
+		nullable: true
+	})
+	updatedAt?: Date | null;
+
+	@DeleteDateColumn({
+		name: 'deleted_at', 
+		type: 'timestamp', 
+		nullable: true
+	})
+	deletedAt?: Date | null;
+
+	// @ManyToOne(() => User)
 	// @JoinColumn({ name: 'user_id' })
-	// userEntity: UserEntity;
+	// user: User;
 
-	// @ManyToOne(() => RestaurantEntity)
-	// @JoinColumn({ name: 'restaurant_id' })
-	// restaurantEntity: RestaurantEntity;
-
-	@CreateDateColumn()
-	created_at: Date;
-
-	@UpdateDateColumn()
-	updated_at: Date;
-
-	@DeleteDateColumn()
-	deleted_at: Date;
+	@ManyToOne(() => RestaurantReviewAggregation)
+	@JoinColumn({ name: 'restaurant_review_aggregation_id' })
+	restaurantReviewAggregation: RestaurantReviewAggregation;
 }
