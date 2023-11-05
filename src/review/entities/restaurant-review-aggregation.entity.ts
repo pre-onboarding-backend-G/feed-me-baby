bf56580ed8fd9c59d9930128bf08878512794afe
Refi Go <64242999+refigo@users.noreply.github.com>
2023-11-05 12:54:00 +0900

    Refactor : Update Entities in Review #6

--- src/review/entities/restaurant-review-aggregation.entity.ts
+++ src/review/entities/restaurant-review-aggregation.entity.ts
@@ -1,37 +1,54 @@
-import { Column, CreateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
 
+@Entity()
+// @Unique('restaurant') //FIXME - Restaurant 엔티티 병합되면 해제할 것
 export class RestaurantReviewAggregation {
-	@PrimaryGeneratedColumn({ name: 'id', type: 'bigint' })
-	id: number;
+  @PrimaryGeneratedColumn({ name: 'id', type: 'bigint' })
+  id: number;
 
-	@Column({ name: 'position', type: 'varchar'})
-	content: string;
-	
-	@Column({type: "decimal", precision: 10, scale: 2, default: 0})
-	average_score: number;
-	
-	@CreateDateColumn({
-		name: 'created_at',
-		type: 'timestamp',
-		nullable: false,
-	})
-	createdAt: Date;
+  @Column({
+    name: 'average_score',
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+    default: 0
+  })
+  averageScore: number;
 
-	@UpdateDateColumn({
-		name: 'updated_at', 
-		type: 'timestamp', 
-		nullable: true
-	})
-	updatedAt?: Date | null;
+  @Column({ name: 'total_count', type: 'integer' })
+  totalCount: number;
 
-	@DeleteDateColumn({
-		name: 'deleted_at', 
-		type: 'timestamp', 
-		nullable: true
-	})
-	deletedAt?: Date | null;
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    nullable: false,
+  })
+  createdAt: Date;
 
-	// @OneToOne(() => Restaurant)
-	// @JoinColumn({ name: 'restaurant_id' })
-	// restaurant: Restaurant;
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    nullable: true,
+  })
+  updatedAt?: Date | null;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamp',
+    nullable: true,
+  })
+  deletedAt?: Date | null;
+
+  //FIXME - Restaurant 엔티티 병합되면 해제할 것
+  // @OneToOne(() => Restaurant)
+  // @JoinColumn({ name: 'restaurant_id' })
+  // restaurant: Restaurant;
 }
