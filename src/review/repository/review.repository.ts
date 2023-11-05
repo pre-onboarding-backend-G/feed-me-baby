ecaa69225a57b5556a3643dcea7c3a58be4c5292
Refi Go <64242999+refigo@users.noreply.github.com>
2023-11-05 09:53:53 +0900

    Feat : Add ReviewRepository #6

---
+++ src/review/repository/review.repository.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Review } from '../entities/review.entity';
+
+@Injectable()
+export class ReviewRepository {
+  constructor(
+    @InjectRepository(Review) private readonly reviewRepository: Repository<Review>,
+  ) {}
+
+  async createRepository(review: Review): Promise<Review> {
+    return await this.reviewRepository.save(review);
+  }
+
+  /**
+   * @author 미종
+   * @param userId Review의 User에 대한 FK 입니다.
+   * @returns Review 엔티티 배열을 Promise로 반환합니다. review가 없을 경우 빈 배열을 반환합니다.
+   */
+  async findReviewsByUser(userId: number): Promise<Review[]> {
+	return await this.reviewRepository.find({
+		where: {
+			// user: {
+			// 	id: userId
+			// }
+		}
+	});
+  }
+
+  /**
+   * @author 미종
+   * @param restaurantId RestaurantReviewAggregation의 Restaurant에 대한 FK 입니다.
+   * @returns Review 엔티티 배열을 Promise로 반환합니다. review가 없을 경우 빈 배열을 반환합니다.
+   */
+  async findReviewsByRestaurant(restaurantId: number): Promise<Review[]> {
+	return await this.reviewRepository.find({
+		where: {
+			restaurantReviewAggregation: {
+				// restaurant: {
+				// 	id: restaurantId
+				// }
+			}
+		}
+	});
+  }
+
+  async updateReview(reviewId: number, review: Review): Promise<boolean> {
+    const result = await this.reviewRepository.update({ id: reviewId }, review);
+    return result.affected > 0;
+  }
+}
