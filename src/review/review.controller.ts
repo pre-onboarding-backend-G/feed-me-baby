cdd3794e9fe13baec8c108ad2f3a7568ebc3f011
Refi Go <64242999+refigo@users.noreply.github.com>
2023-11-06 17:17:09 +0900

    Feat : ReviewController에서 UserId 데코레이터를 이용하도록 구현 #23

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -7,6 +7,8 @@ import { ReviewService } from './review.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { SwaggerCreateReview } from './decorator/swagger/create-review.decorator';
+import { UserId } from 'src/auth/decorator/user-id.decorator';
+import { ResponseEntity } from 'src/common/response.entity';
 
 @ApiTags('reviews')
 @Controller('reviews')
@@ -17,14 +19,10 @@ export class ReviewController {
   @SwaggerCreateReview()
   @Post()
   async create(
-    // @UserId() userId: number, //FIXME - 병합 후 사용 가능 확인 후 해제할 것
+    @UserId() userId: number,
     @Body() dto: CreateReviewDto,
-  )
-  // : Promise<ResponseEntity<string> //FIXME - 병합 후 사용 가능 확인 후 해제할 것
-  {
-    let userId: number = 1; // tmp to implement //FIXME - 병합 후 userId 사용 가능 확인 후 삭제할 것
-    await this.reviewService.create(userId, dto.score, dto.content, dto.restaurantId);
-    // return RepositoryEntity.OK('리뷰 작성 요청에 성공했습니다') //FIXME - 병합 후 사용 가능 확인 후 해제할 것
-    return ;
+  ): Promise<ResponseEntity<string>> {
+    await this.reviewService.create(userId, dto.toEntity(), dto.restaurantUniqueId);
+    return ResponseEntity.OK('리뷰 작성 요청에 성공했습니다');
   }
 }
