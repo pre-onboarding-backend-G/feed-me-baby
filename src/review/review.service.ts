94926bb6627ea69c9fdf5fcf34bc19bfa58f4de8
Refi Go <64242999+refigo@users.noreply.github.com>
2023-11-06 17:16:13 +0900

    Refactor : ReviewService의 create, getAverageScoreAndTotalCountByRestaurantId 메서드 재작성 #23

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ReviewRepository } from './repository/review.repository';
 import { RestaurantreviewAggregationRepository } from './repository/restaurant-review-aggregation.repository';
 import { RestaurantReviewAggregation } from './entities/restaurant-review-aggregation.entity';
@@ -9,69 +9,51 @@ export class ReviewService {
   constructor(
     private readonly reviewRepository
     : ReviewRepository,
-    private readonly restaurantReviewAggregatioRepository
-    : RestaurantreviewAggregationRepository
+    private readonly restaurantReviewAggregationRepository
+    : RestaurantreviewAggregationRepository,
   ) {}
 
-  private async getRestaurantReviewAggregationByRestaurantId(restaurantId: number) {
-    let rra: RestaurantReviewAggregation = await this
-      .restaurantReviewAggregatioRepository
-      .findByRestaurantId(restaurantId);
-    if (rra === null) {
-      //FIXME - 개발 중에 아직 Restaurant 엔티티가 존재하지 않아서 주석 처리함, 병합 후에 해제할 것
-      // let restaurant = await this.restaurantRepository(restaurantId);
-      // if (restaurant === null) {
-      //   throw new NotFoundException('해당 식당이 존재하지 않습니다')
-      // }
-      // rra = await this.restaurantReviewAggregatioRepository.createRepository({
-      //   averageScore: 0,
-      //   totalCount: 0,
-      //   restaurant: restaurant
-      // });
-    }
-    return rra;
+  private async getRestaurantReviewAggregationByRestaurantUniqueId(
+    restaurantUniqueId: string
+  ): Promise<RestaurantReviewAggregation> {
+    const restaurantReviewAggregation: RestaurantReviewAggregation
+      = await this.restaurantReviewAggregationRepository
+      .findByRestaurantUniqueId(restaurantUniqueId);
+    return restaurantReviewAggregation;
   }
 
-  private async makeReview(
-    userId: number, 
-    score: number, 
-    content: string, 
-    restaurantId: number, 
-    rra: RestaurantReviewAggregation
-  ): Promise<Review> {
-    let userReview: Review = await 
-      this.reviewRepository
-      .findReviewsByUserIdAndRestaurantId(userId, restaurantId);
-    if (userReview !== null) {
-      throw new ConflictException('이미 해당 식당에 대해 작성한 리뷰가 있습니다');
-    }
-    userReview = this.reviewRepository.createRepository(
-      score,
-      content,
-      rra
-    );
-    userReview = await this.reviewRepository.saveRepository(userReview);
-    return userReview;
-  }
-
-  private async updateRestaurantReviewAverageScore(
-    rra: RestaurantReviewAggregation, 
+  private async updateRestaurantReviewAverageScoreAndTotalCount(
+    restaurantReviewAggregation: RestaurantReviewAggregation, 
     userReview: Review
   ): Promise<void> {
-    rra.averageScore 
-      = (rra.averageScore * rra.totalCount + userReview.score) / (rra.totalCount + 1);
-    rra.totalCount = rra.totalCount + 1;
-    await this.restaurantReviewAggregatioRepository.updateRepository(rra.id, rra);
-    return ;
+    const prevAverageScore: number = restaurantReviewAggregation.averageScore;
+    const prevTotalCount: number = restaurantReviewAggregation.totalCount;
+    restaurantReviewAggregation.averageScore 
+      = ((prevAverageScore * prevTotalCount) + userReview.score)
+        / (prevTotalCount + 1);
+    restaurantReviewAggregation.totalCount = prevTotalCount + 1;
+    await this.restaurantReviewAggregationRepository
+      .updateRepository(
+        restaurantReviewAggregation.id, 
+        restaurantReviewAggregation
+    );
   }
 
-  async create(userId: number, score: number, content: string, restaurantId: number) {
-    const rra: RestaurantReviewAggregation
-      = await this.getRestaurantReviewAggregationByRestaurantId(restaurantId);
-    const userReview: Review
-      = await this.makeReview(userId, score, content, restaurantId, rra);
-    await this.updateRestaurantReviewAverageScore(rra, userReview);
-    return ;
+  async create(
+    userId: number, review: Review, restaurantUniqueId: string
+  ): Promise<void> {
+    review.userId = userId;
+    const restaurantReviewAggregation: RestaurantReviewAggregation
+      = await this
+      .getRestaurantReviewAggregationByRestaurantUniqueId(
+        restaurantUniqueId
+    );
+    review.restaurantReviewAggregationId = restaurantReviewAggregation.id;
+    await this.reviewRepository.save(review);
+    await this.updateRestaurantReviewAverageScoreAndTotalCount(
+      restaurantReviewAggregation,
+      review
+    );
   }
 
   async findReviewsByUserId(userId: number): Promise<Review[]> {
@@ -82,15 +64,14 @@ export class ReviewService {
     return await this.reviewRepository.findReviewsByRestaurantId(restaurantId);
   }
 
-  async getAverageScoreAndTotalCountByRestaurantId(restaurantId: number)
+  async getAverageScoreAndTotalCountByRestaurantId(restaurantUniqueId: string)
   : Promise<{ averageScore: number, totalCount: number }> {
-    const rra: RestaurantReviewAggregation 
-      = await this.getRestaurantReviewAggregationByRestaurantId(
-        restaurantId
-    );
+    let restaurantReviewAggregation: RestaurantReviewAggregation
+      = await this.restaurantReviewAggregationRepository
+      .findByRestaurantUniqueId(restaurantUniqueId);
     return {
-      averageScore: rra.averageScore,
-      totalCount: rra.totalCount
+      averageScore: restaurantReviewAggregation.averageScore,
+      totalCount: restaurantReviewAggregation.totalCount
     }
   }
 }
