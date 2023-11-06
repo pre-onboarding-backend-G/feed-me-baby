88597f7c2b63dbb92d3337d499a3656a99f3ea64
myeongseoklee <yhkyhk92@gmail.com>
2023-11-06 23:37:29 +0900

    Rename : request dto 테스트 코드 파일명 및 테스트 코드 import문 수정

--- src/user/dto/update-user.dto.spec.ts
+++ src/user/dto/update-user.dto.spec.ts
@@ -1,12 +1,12 @@
-import { UpdateUserReqDto } from './update-user-req.dto';
 import { User } from '../entity/user.entity';
+import { UpdateUserDto } from './update-user.dto';
 
 describe('sign-up-req.dto.ts', () => {
   describe('UpdateUserReqDto', () => {
     describe('toEntity', () => {
       it('SUCCESS: Request Body에 올바른 값이 전달되었을 때 그 필드들을 가진 user 엔티티 리턴', () => {
         // When
-        const updateUserReqDto = new UpdateUserReqDto();
+        const updateUserReqDto = new UpdateUserDto();
         updateUserReqDto.latitude = 37.566295;
         updateUserReqDto.longitude = 126.977945;
         updateUserReqDto.isRecommendateLunch = true;
